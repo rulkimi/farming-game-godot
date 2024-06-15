550deffb6ed39a6b550be32815d8abb785491077
rulkimi <azrulkimi00@gmail.com>
2024-06-15 13:57:40 +0800

    added code to change tools

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,6 +15,19 @@ func _ready():
 func _physics_process(delta):
 	player_movement(delta)
 	
+	if Input.is_action_pressed("tool_1"):
+		current_tool = "axe"
+		$AnimatedSprite2D.frame = 0
+		$AnimatedSprite2D.play("front_axe")
+	elif Input.is_action_pressed("tool_2"):
+		current_tool = "hoe"
+		$AnimatedSprite2D.frame = 0
+		$AnimatedSprite2D.play("front_hoe")
+	elif Input.is_action_pressed("tool_3"):
+		current_tool = "watering-pot"
+		$AnimatedSprite2D.frame = 0
+		$AnimatedSprite2D.play("front_watering-pot")
+	
 func player_movement(delta):
 	if Input.is_action_pressed("ui_right") and !using_tool:
 		is_moving = true
@@ -76,5 +89,19 @@ func _on_AnimatedSprite2D_animation_finished():
 		using_tool = false
 	else:
 		is_moving = false
+		
+func get_animation(direction):
+	match direction:
+		"right":
+			$AnimatedSprite2D.flip_h = false
+			return "side"
+		"left":
+			$AnimatedSprite2D.flip_h = true
+			return "side"
+		"down":
+			return "front"
+		"up":
+			return "back"
+			
 
 
