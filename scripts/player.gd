0a4547e203a3d236b230291849b01c14b73bfb78
rulkimi <azrulkimi00@gmail.com>
2024-06-15 16:05:20 +0800

    code refactor for player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,95 +1,63 @@
 extends CharacterBody2D
 
-var speed = 100
+@export var speed = 100
 
 var using_tool = false
 var current_tool = "hoe"
-
 var is_moving = false
-var current_direction = "none"
+var current_direction = "down"
+
+const TOOLS = {"tool_1": "axe", "tool_2": "hoe", "tool_3": "watering-pot"}
+const DIRECTIONS = {"ui_right": Vector2(1, 0), "ui_left": Vector2(-1, 0), "ui_down": Vector2(0, 1), "ui_up": Vector2(0, -1)}
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 	$AnimatedSprite2D.connect("animation_finished", _on_AnimatedSprite2D_animation_finished)
 
 func _physics_process(delta):
-	player_movement(delta)
-	
-	if Input.is_action_pressed("tool_1"):
-		current_tool = "axe"
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play("front_axe")
-	elif Input.is_action_pressed("tool_2"):
-		current_tool = "hoe"
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play("front_hoe")
-	elif Input.is_action_pressed("tool_3"):
-		current_tool = "watering-pot"
-		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play("front_watering-pot")
-	
-func player_movement(delta):
-	if Input.is_action_pressed("ui_right") and !using_tool:
-		is_moving = true
-		current_direction = "right"
-		velocity.x = speed
-		velocity.y = 0
-		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.play("side_walk")
-	elif Input.is_action_pressed("ui_left") and !using_tool:
-		is_moving = true
-		current_direction = "left"
-		velocity.x = -speed
-		velocity.y = 0
-		$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("side_walk")
-	elif Input.is_action_pressed("ui_down") and !using_tool:
-		is_moving = true
-		current_direction = "down"
-		velocity.x = 0
-		velocity.y = speed
-		$AnimatedSprite2D.play("front_walk")
-	elif Input.is_action_pressed("ui_up") and !using_tool:
-		is_moving = true
-		current_direction = "up"
-		velocity.x = 0
-		velocity.y = -speed
-		$AnimatedSprite2D.play("back_walk")
-	elif Input.is_action_pressed("use_tool") and !is_moving:
-		using_tool = true
-		if current_direction == "right":
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("side_" + current_tool)
-		elif current_direction == "left":
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("side_" + current_tool)
-		elif current_direction == "down":
-			$AnimatedSprite2D.play("front_" + current_tool)
-		elif current_direction == "up":
-			$AnimatedSprite2D.play("back_" + current_tool)
-	elif !using_tool:
-		is_moving = false
-		velocity.x = 0
-		velocity.y = 0
-		if current_direction == "right":
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("side_idle")
-		elif current_direction == "left":
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("side_idle")
-		elif current_direction == "down":
-			$AnimatedSprite2D.play("front_idle")
-		elif current_direction == "up":
-			$AnimatedSprite2D.play("back_idle")
-		
+	handle_movement_input()
+	handle_tool_input()
+	handle_tool_usage()
 	move_and_slide()
-	
+
+func handle_movement_input():
+	if !using_tool:
+		for action in DIRECTIONS.keys():
+			if Input.is_action_pressed(action):
+				is_moving = true
+				current_direction = action.replace("ui_", "")
+				$AnimatedSprite2D.flip_h = (action == "ui_left")
+				velocity = DIRECTIONS[action] * speed
+				$AnimatedSprite2D.play(get_animation(current_direction) + "_walk")
+				return
+	stop_moving()
+
+func handle_tool_input():
+	for tool_action in TOOLS.keys():
+		if Input.is_action_pressed(tool_action):
+			current_tool = TOOLS[tool_action]
+			$AnimatedSprite2D.frame = 0
+			$AnimatedSprite2D.play("front" + "_" + current_tool)
+			return
+
+func handle_tool_usage():
+	if Input.is_action_just_pressed("use_tool") and !is_moving:
+		using_tool = true
+		$AnimatedSprite2D.flip_h = (current_direction == "left")
+		$AnimatedSprite2D.play(get_animation(current_direction) + "_" + current_tool)
+
+func stop_moving():
+	if !using_tool:
+		is_moving = false
+		velocity = Vector2.ZERO
+		$AnimatedSprite2D.play(get_animation(current_direction) + "_idle")
+
 func _on_AnimatedSprite2D_animation_finished():
-	if $AnimatedSprite2D.animation == "side_" + current_tool or "back_" + current_tool or "front_" + current_tool:
+	if $AnimatedSprite2D.animation == get_animation(current_direction) + "_" + current_tool:
 		using_tool = false
 	else:
 		is_moving = false
-		
+
 func get_animation(direction):
 	match direction:
 		"right":
@@ -102,6 +70,4 @@ func get_animation(direction):
 			return "front"
 		"up":
 			return "back"
-			
-
-
+	return "front"
