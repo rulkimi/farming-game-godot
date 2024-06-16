2e6a8e07ebf9ca2609b2df8410bdb1eb82a898f0
rulkimi <azrulkimi00@gmail.com>
2024-06-16 15:27:15 +0800

    the hoe shouldnt change the watered hoed land

--- scripts/world.gd
+++ scripts/world.gd
@@ -8,10 +8,10 @@ func _ready():
 
 func _on_player_tool_used(position: Vector2, tool: String):
 	var tile_map_pos: Vector2i = tile_map.local_to_map(position)
-	if tool == "hoe":
+	if tool == "hoe" and tile_map.get_cell_atlas_coords(5, tile_map_pos) != Vector2i(0, 0):
 		tile_map.set_cell(5, tile_map_pos, 12, Vector2i(0, 0))
 	elif tool == "watering-pot":
-	# Check if the current tile is hoed (cell ID 12)
+	# Check if the current tile is hoed
 		if tile_map.get_cell_atlas_coords(5, tile_map_pos) == Vector2i(0, 0):
 			tile_map.set_cell(5, tile_map_pos, 11, Vector2i(0, 0))
 
